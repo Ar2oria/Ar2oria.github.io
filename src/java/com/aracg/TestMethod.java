f12742e63048c00fb26fdf00cb19dd9d2f096595
Ar2oria <ex1stance@outlook.com>
2018-09-24 11:40:26 +0800

    collection

--- src/java/com/aracg/TestMethod.java
+++ src/java/com/aracg/TestMethod.java
@@ -2,12 +2,7 @@ package com.aracg;
 
 
 import java.util.AbstractList;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingDeque;
-import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class TestMethod {
     public static void main(String[] args) {
